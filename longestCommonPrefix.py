ef060077f8b0833883b673482d76a759304eaf37
Taycir Yahmed <taycir.yahmed@gmail.com>
2019-04-04 22:55:55 +0200

    Update longestCommonPrefix.py

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -4,16 +4,17 @@ class longestCommonPrefix(object):
         :type strs: List[str]
         :rtype: str
         """
-        if len(strs) ==0 : return ""
-        
+        if len(strs) == 0:
+            return ""
+
         pref = strs[0]
-        
+
         i = 1
-        
+
         while (i < len(strs)) and (pref != ""):
             while (pref != "") and not strs[i].startswith(pref):
                 pref = pref[:-1]
-                
-            i+=1
-        
+
+            i += 1
+
         return pref
