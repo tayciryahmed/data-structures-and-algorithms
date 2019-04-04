dd9f90bbc45bbccf971df7f31194335ebcd481eb
Taycir Yahmed <taycir.yahmed@gmail.com>
2019-04-04 23:22:18 +0200

    Update longestCommonPrefix.py

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -18,3 +18,18 @@ class longestCommonPrefix(object):
             i += 1
 
         return pref
+
+    def horizontal_scanning(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs) == 0:
+            return ""
+        pref = strs[0]
+
+        for i in range(1, len(strs)):
+            while (pref != "") and not strs[i].startswith(pref):
+                pref = pref[:-1]
+
+        return pref
