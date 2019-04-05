9826e68b2613a6ace11c91f0cf1d1efc459bf2e9
Taycir Yahmed <taycir.yahmed@gmail.com>
2019-04-05 22:34:35 +0200

    Update longestCommonPrefix.py

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -64,3 +64,72 @@ class longestCommonPrefix(object):
             return strs[0][:pref+1]
 
         return ""
+
+    def vertical_scanning(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+
+        if (len(strs) == 0) or (strs is None):
+            return ""
+
+        for i in range(len(strs[0])):
+            c = strs[0][i]
+            for j in range(len(strs)):
+                if (i == len(strs[j])) or (strs[j][i] != c):
+                    return strs[0][:i]
+
+        return strs[0]
+
+     def longestCommonPrefix_strings(self, str_1, str_2):
+        c = ""
+        min_len = min(len(str_1), len(str_2))
+        for i in range(min_len):
+            if str_1[i] == str_2[i]:
+                c+=str_1[i]
+            else:
+                return c
+        return c
+        
+    def my_devide_conquer(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        
+        if len(strs) == 0 : 
+            return ""
+        elif len(strs) == 1: 
+            return strs[0]
+        elif len(strs)==2:
+            return self.longestCommonPrefix_strings(strs[0], strs[1])
+        else:
+            mid = len(strs)/2
+            print (mid)
+            return self.longestCommonPrefix([self.longestCommonPrefix(strs[:mid]),\
+                                            self.longestCommonPrefix(strs[mid:])])
+    
+    def dvide_conquer(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs) == 0: return ""
+        return self.longestCommonPrefix_(strs, 0, len(strs)-1)
+    
+    def longestCommonPrefix_(self, strs, l, r):
+        if l==r: return strs[l]
+        else:
+            mid = (l+r)/2
+            lcpLeft = self.longestCommonPrefix_(strs, l, mid)
+            lcpRight = self.longestCommonPrefix_(strs, mid+1, r)
+            return self.commonPrefix(lcpRight, lcpLeft)
+    
+    def commonPrefix(self, left, right):
+        min_len = min (len(left), len(right))
+        for i in range(min_len):
+            if right[i]!=left[i]:
+                return left[:i]
+        
+        return left[:min_len]
