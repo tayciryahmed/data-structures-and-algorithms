34f0cb23bcae70ba6304ddb2157601d6a4013478
Taycir Yahmed <taycir.yahmed@gmail.com>
2019-04-08 14:48:19 +0200

    Create valid-parentheses.py

---
+++ valid-parentheses.py
@@ -0,0 +1,28 @@
+class isValid(object):
+        
+    def my_solution(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        opening = ['(','[','{']
+        closing = [')',']','}']
+        waiting_for = []
+        
+        if len(s) == 0 : return True 
+        if len(s) == 1 : return False 
+        if s[0] in closing: return False 
+        if s[-1] in opening : return False
+        if len(s)%2 == 1 : return False
+        
+        for c in s:
+            if c in opening :
+                waiting_for.append(closing [opening.index (c)])
+            else:
+                if c == waiting_for[-1]:
+                    del waiting_for[-1]
+                else:
+                    return False
+        
+        if len(waiting_for) == 0: return True 
+        else: return False 
