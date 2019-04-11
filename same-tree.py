0264e1b1df2b45d28f5f13e27062156bb4c992cf
Taycir Yahmed <taycir.yahmed@gmail.com>
2019-04-11 14:39:54 +0200

    Update same-tree.py

--- same-tree.py
+++ same-tree.py
@@ -40,3 +40,23 @@ class Solution(object):
             return False
         
         return None 
+    
+    def solIsSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        
+        if (not p) and (not q):
+            return True
+        
+        if (not p) or (not q):
+            return False 
+        
+        if p.val != q.val :
+            return False 
+            
+        return self.isSameTree(p.left, q.left) and \
+               self.isSameTree(p.right, q.right)
+        
