f8cfa023129e49a5eb865d855df3738a736342f8
Taycir Yahmed <taycir.yahmed@gmail.com>
2019-04-11 14:31:14 +0200

    Create same-tree.py

---
+++ same-tree.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+
+class Solution(object):
+    def isLeaf(self, p):
+        return (p.left == None) and (p.right == None)
+    
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        
+        if (not p) and (not q):
+            return True
+        
+        elif p and not q:
+            return False
+        
+        elif q and not p: 
+            return False 
+            
+        if self.isLeaf(p) or self.isLeaf(q):
+            if self.isLeaf(q) and self.isLeaf(p) and (p.val == q.val):
+                return True 
+            else: 
+                return False 
+            
+        elif p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        else:
+            return False
+        
+        return None 
