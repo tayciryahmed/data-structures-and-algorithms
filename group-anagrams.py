9f0cb52f632e0268fc71d25f3c1795214860c636
Taycir Yahmed <taycir.yahmed@gmail.com>
2020-08-16 14:49:49 +0200

    Create group-anagrams.py

---
+++ group-anagrams.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        strs = [(x, ''.join(sorted(x))) for x in strs]
+        strs.sort(key=lambda x: x[1])
+        
+        
+        ans = [[strs[0][0]]]
+            
+        for i in range(1, len(strs)):
+            if strs[i][1] == strs[i-1][1]:
+                ans[-1].append(strs[i][0])
+            else:
+                ans.append([strs[i][0]])
+        
+        return ans 
+            
