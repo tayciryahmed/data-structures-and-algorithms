30ee173edc09f86e532e8e1229b8a591d1a9f167
Taycir Yahmed <taycir.yahmed@gmail.com>
2020-08-16 17:55:38 +0200

    Create integer-break.py

---
+++ integer-break.py
@@ -0,0 +1,10 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        
+        dp = [1] * (n+1)
+        
+        for i in range(3, n+1):
+            for j in range(1, i//2+1):
+                dp[i] = max(dp[i], j*(i-j), j*dp[i-j])
+        
+        return dp[n]
