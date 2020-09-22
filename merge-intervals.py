0ac84f11fc0cbaa3084d873d96e2380632208f76
Taycir Yahmed <taycir.yahmed@gmail.com>
2020-09-22 17:02:12 +0200

    Update merge-intervals.py

--- merge-intervals.py
+++ merge-intervals.py
@@ -16,3 +16,21 @@ class Solution:
         
         return ans
             
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        if len(intervals) == 0: return []
+
+        intervals.sort(key=lambda x: x[0])
+    
+        ans = [intervals[0]]
+        
+        for i in range(1, len(intervals)):
+            if intervals[i][0] <= ans[-1][1]:
+                if intervals[i][1] > ans[-1][1]:
+                    ans[-1][1] =  intervals[i][1]
+            else:
+                ans.append(intervals[i])
+            
+        return ans 
