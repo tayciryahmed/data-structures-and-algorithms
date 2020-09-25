034cfc3286630454a6bd62c237491272ed22bdc3
Taycir Yahmed <taycir.yahmed@gmail.com>
2020-09-25 12:18:05 +0200

    Update valid-square.py

--- valid-square.py
+++ valid-square.py
@@ -7,4 +7,4 @@ class Solution:
         
         p.sort(key=lambda x: (x[0], x[1]))
         
-        return dist(p[0], p[1]) != 0 and dist(p[0], p[1]) == dist(p[1], p[3]) and dist(p[1], p[3]) == dist(p[3], p[2]) and dist(p[3], p[2]) == dist(p[2], p[0])   and dist(p[0],p[3])==dist(p[1],p[2])
+        return dist(p[0], p[1]) != 0 and dist(p[0], p[1]) == dist(p[1], p[3]) == dist(p[3], p[2]) == dist(p[2], p[0])   and dist(p[0],p[3]) == dist(p[1],p[2])
