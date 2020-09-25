a3f90ee0e8faf470743ffb542fb449d051613646
Taycir Yahmed <taycir.yahmed@gmail.com>
2020-09-25 12:15:19 +0200

    Create valid-square.py

---
+++ valid-square.py
@@ -0,0 +1,10 @@
+class Solution:
+    def validSquare(self, p1: List[int], p2: List[int], p3: List[int], p4: List[int]) -> bool:
+        def dist(p1, p2):
+            return (p2[1]-p1[1])**2 + (p2[0]-p1[0])**2
+        
+        p = [p1, p2, p3, p4]
+        
+        p.sort(key=lambda x: (x[0], x[1]))
+        
+        return dist(p[0], p[1]) != 0 and dist(p[0], p[1]) == dist(p[1], p[3]) and dist(p[1], p[3]) == dist(p[3], p[2]) and dist(p[3], p[2]) == dist(p[2], p[0])   and dist(p[0],p[3])==dist(p[1],p[2])
